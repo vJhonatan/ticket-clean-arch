7a5f377ebd9cae36112a02056d2d9f38bd676eab
vJhonatan <jhonatan99653@gmail.com>
2024-12-17 19:43:26 -0300

    feat: usecases implements

---
+++ src/main/java/com/project/ticket/core/usecases/newEventUsecase.java
@@ -0,0 +1,9 @@
+package com.project.ticket.core.usecases;
+
+import com.project.ticket.core.domain.Event;
+
+public interface newEventUsecase {
+
+    public Event execute(Event event);
+
+}
---
+++ src/main/java/com/project/ticket/core/usecases/newEventUsecaseImpl.java
@@ -0,0 +1,11 @@
+package com.project.ticket.core.usecases;
+
+import com.project.ticket.core.domain.Event;
+
+public class newEventUsecaseImpl implements newEventUsecase {
+
+    @Override
+    public Event execute(Event event) {
+        return null;
+    }
+}
